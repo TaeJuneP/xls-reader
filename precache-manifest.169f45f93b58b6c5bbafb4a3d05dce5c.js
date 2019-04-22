2d5b4f39d64dd52012311a3278509d17b56aa2ec
TaeJuneP <35757581+TaeJuneP@users.noreply.github.com>
2019-04-22 18:54:46 +0900

    Updates

---
+++ precache-manifest.169f45f93b58b6c5bbafb4a3d05dce5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c80e4c8ef139c3d0175a",
+    "url": "/TaeJuneP/xls-reader/static/js/runtime~main.2a4d1968.js"
+  },
+  {
+    "revision": "dff20541a795360829d0",
+    "url": "/TaeJuneP/xls-reader/static/js/main.01ae00f3.chunk.js"
+  },
+  {
+    "revision": "58909e9a8c6d8075499c",
+    "url": "/TaeJuneP/xls-reader/static/js/2.de5886c3.chunk.js"
+  },
+  {
+    "revision": "dff20541a795360829d0",
+    "url": "/TaeJuneP/xls-reader/static/css/main.26b4b298.chunk.css"
+  },
+  {
+    "revision": "1b4c64365db5eee1d29e4525b3423134",
+    "url": "/TaeJuneP/xls-reader/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/TaeJuneP/xls-reader/precache-manifest.169f45f93b58b6c5bbafb4a3d05dce5c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/TaeJuneP/xls-reader/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
