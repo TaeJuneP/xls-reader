cb3ce1f6d4d146db2ebc6992d3b716ba62291a78
TaeJuneP <35757581+TaeJuneP@users.noreply.github.com>
2019-04-22 20:14:45 +0900

    refactoring

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "./App.css";
 import CSVReader from "react-csv-reader";
 import { CSVLink, CSVDownload } from "react-csv";
 import styled from "styled-components";
+import { ToastContainer, toast } from "react-toastify";
+
 class App extends Component {
   state = {
     before: "",
@@ -11,56 +13,73 @@ class App extends Component {
     inputdata: [],
     filename: ""
   };
+  notify = () =>
+    toast("변경 완료 다운 받으세요", {
+      position: "bottom-center",
+      autoClose: 1000,
+      hideProgressBar: true,
+      closeOnClick: false,
+      pauseOnHover: false,
+      draggable: false
+    });
   render() {
     return (
-      <Container>
-        <CSVReader
-          cssClass="react-csv-input"
-          label="CSV파일 선택"
-          onFileLoaded={this.handleForce}
-        />
-        {this.handleList()}
-        <Input
-          placeholder="칼럼번호"
-          value={this.state.col}
-          onChange={this.handleCol}
-        />
-        <Input
-          placeholder="변경 전"
-          value={this.state.before}
-          onChange={this.handleBefore}
-        />
-        <Input
-          placeholder="변경 후"
-          value={this.state.after}
-          onChange={this.handleAfter}
-        />
-        <Input
-          placeholder="생성 파일명"
-          value={this.state.filename}
-          onChange={this.handleName}
-        />
-        <Button
-          onClick={() => {
-            this.OutputButton(
-              this.state.inputdata,
-              this.state.before,
-              this.state.after,
-              this.state.col
-            );
-          }}
-        >변경하기</Button>
-        <LickContainer>
-        <CSVLink
-          data={this.state.inputdata}
-          filename={this.state.filename + ".csv"}
-          className="buttonhi"
-          target="_blank"
-        >
-          파일 다운로드
-        </CSVLink>
-        </LickContainer>
-      </Container>
+      <>
+              <ToastContainer />
+
+      <Back>
+        <Container>
+          <CSVReader
+            cssClass="react-csv-input"
+            label="CSV파일 선택"
+            onFileLoaded={this.handleForce}
+          />
+          {this.handleList()}
+          <Input
+            placeholder="칼럼번호"
+            value={this.state.col}
+            onChange={this.handleCol}
+          />
+          <Input
+            placeholder="변경 전"
+            value={this.state.before}
+            onChange={this.handleBefore}
+          />
+          <Input
+            placeholder="변경 후"
+            value={this.state.after}
+            onChange={this.handleAfter}
+          />
+          <Input
+            placeholder="생성 파일명"
+            value={this.state.filename}
+            onChange={this.handleName}
+          />
+          <Button
+            onClick={() => {
+              this.OutputButton(
+                this.state.inputdata,
+                this.state.before,
+                this.state.after,
+                this.state.col
+              );
+            }}
+          >
+            변경하기
+          </Button>
+          <LickContainer>
+            <CSVLink
+              data={this.state.inputdata}
+              filename={this.state.filename + ".csv"}
+              className="buttonhi"
+              target="_blank"
+            >
+              파일 다운로드
+            </CSVLink>
+          </LickContainer>
+        </Container>
+      </Back>
+      </>
     );
   }
   handleForce = data => {
@@ -107,7 +126,11 @@ class App extends Component {
         data[i][col] = after;
       }
     }
-    console.log(data);
+    this.setState({
+      inputdata: data
+    });
+    this.notify();
+    console.log(this.state.inputdata);
   };
 }
 const DataList = styled.div`
@@ -123,21 +146,26 @@ const Container = styled.div`
 const Input = styled.input`
   height: 50px;
   width: 100%;
-  margin-top:5px;
+  margin-top: 5px;
 `;
 const LickContainer = styled.div`
-  height:40px;
-  width:150px;
-  margin:auto;
-  text-align:center;
-  justify-content:center;
-`
+  height: 40px;
+  width: 150px;
+  margin: auto;
+  text-align: center;
+  justify-content: center;
+`;
 const Button = styled.button`
   height: 40px;
   width: 150px;
   display: block;
   margin: auto;
-  font-size:16px;
-  margin-top:5px;
+  font-size: 16px;
+  margin-top: 5px;
+`;
+const Back = styled.div`
+  background-image: linear-gradient(to bottom right, #989ade, #5f6ccc);
+  height: 100%;
+  width: 100%;
 `;
 export default App;
