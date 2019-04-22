28ede2643dcc7e2e0b59281a4745523136c75619
taejune park <ptj1554@gmail.com>
2019-04-22 20:30:58 +0900

    refactoring

--- src/App.js
+++ src/App.js
@@ -4,8 +4,20 @@ import CSVReader from "react-csv-reader";
 import { CSVLink, CSVDownload } from "react-csv";
 import styled from "styled-components";
 import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 class App extends Component {
+  notify = () =>{
+    toast("변경 완료 다운 받으세요", {
+      position: "bottom-center",
+      autoClose: 3000,
+      hideProgressBar: true,
+      closeOnClick: false,
+      pauseOnHover: false,
+      draggable: false
+    });
+  }
+  
   state = {
     before: "",
     after: "",
@@ -13,72 +25,63 @@ class App extends Component {
     inputdata: [],
     filename: ""
   };
-  notify = () =>
-    toast("변경 완료 다운 받으세요", {
-      position: "bottom-center",
-      autoClose: 1000,
-      hideProgressBar: true,
-      closeOnClick: false,
-      pauseOnHover: false,
-      draggable: false
-    });
+ 
   render() {
     return (
       <>
-              <ToastContainer />
-
-      <Back>
-        <Container>
-          <CSVReader
-            cssClass="react-csv-input"
-            label="CSV파일 선택"
-            onFileLoaded={this.handleForce}
-          />
-          {this.handleList()}
-          <Input
-            placeholder="칼럼번호"
-            value={this.state.col}
-            onChange={this.handleCol}
-          />
-          <Input
-            placeholder="변경 전"
-            value={this.state.before}
-            onChange={this.handleBefore}
-          />
-          <Input
-            placeholder="변경 후"
-            value={this.state.after}
-            onChange={this.handleAfter}
-          />
-          <Input
-            placeholder="생성 파일명"
-            value={this.state.filename}
-            onChange={this.handleName}
-          />
-          <Button
-            onClick={() => {
-              this.OutputButton(
-                this.state.inputdata,
-                this.state.before,
-                this.state.after,
-                this.state.col
-              );
-            }}
-          >
-            변경하기
-          </Button>
-          <LickContainer>
-            <CSVLink
-              data={this.state.inputdata}
-              filename={this.state.filename + ".csv"}
-              className="buttonhi"
-              target="_blank"
+        <ToastContainer />
+        <Back>
+          <Container>
+            <CSVReader
+              cssClass="react-csv-input"
+              label="CSV파일 선택"
+              onFileLoaded={this.handleForce}
+            />
+            {this.handleList()}
+            <Input
+              placeholder="칼럼번호"
+              value={this.state.col}
+              onChange={this.handleCol}
+            />
+            <Input
+              placeholder="변경 전"
+              value={this.state.before}
+              onChange={this.handleBefore}
+            />
+            <Input
+              placeholder="변경 후"
+              value={this.state.after}
+              onChange={this.handleAfter}
+            />
+            <Input
+              placeholder="생성 파일명"
+              value={this.state.filename}
+              onChange={this.handleName}
+            />
+            <Button
+              onClick={() => {
+                this.OutputButton(
+                  this.state.inputdata,
+                  this.state.before,
+                  this.state.after,
+                  this.state.col
+                );
+              }}
             >
-              파일 다운로드
-            </CSVLink>
-          </LickContainer>
-        </Container>
-      </Back>
+              변경하기
+            </Button>
+            <LickContainer>
+              <CSVLink
+                data={this.state.inputdata}
+                filename={this.state.filename + ".csv"}
+                className="buttonhi"
+                target="_blank"
+              >
+                파일 다운로드
+              </CSVLink>
+            </LickContainer>
+          </Container>
+        </Back>
       </>
     );
   }
@@ -119,8 +122,6 @@ class App extends Component {
     });
   };
   OutputButton = (data, before, after, col) => {
-    console.log(data.length);
-    console.log(data[col]);
     for (var i = 0; i < data.length; i++) {
       if (data[i][col] === before) {
         data[i][col] = after;
@@ -130,13 +131,13 @@ class App extends Component {
       inputdata: data
     });
     this.notify();
-    console.log(this.state.inputdata);
   };
 }
 const DataList = styled.div`
   height: 30px;
   width: 100%;
   border: 1px solid black;
+  background-color:#ffffff;
 `;
 const Container = styled.div`
   height: 100%;
